9abe4f390a40834e7b881dcb8968bf0c208f640c
ruzny <ruzny@ruzny.com>
2020-09-15 21:57:15 +0530

    added a piece of styles for dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import AuthContext from '../context/AuthContext';
 
+// Styles
+import { dashboardStyles } from '../styles/dashboard.module';
+
 const Dashboard = () => {
   const {
     authData: { user },
   } = React.useContext(AuthContext);
 
   return (
-    <div className='dashboard'>
+    <div className='dashboard' style={dashboardStyles.dashboardContainer}>
       <h2>Dashboard</h2>
       {user && <p>{user}</p>}
     </div>
---
+++ src/styles/dashboard.module.js
@@ -0,0 +1,11 @@
+export const dashboardStyles = {
+    dashboardContainer: {
+        width: '60%',
+        height: '80vh',
+        margin: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+};
